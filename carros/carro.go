7942a8fd1a2388a22f5ad0f0d15e3d92bc564438
jpcadinelli <jpcadinelli@gmail.com>
2024-03-21 19:18:30 -0300

    construção do objeto carro

--- carros/carro.go
+++ carros/carro.go
@@ -1,5 +1,13 @@
 package carros
 
 type carro struct {
-	
+	Marca string
+	Modelo string
+	Ano int
+	Cor string
+	VelocidadeAtual float64
+}
+
+func (car *carro) Acelerar(quantidade float64) {
+	car.VelocidadeAtual += quantidade
 }
